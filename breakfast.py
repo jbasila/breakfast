c00cbdda4390e6b666aa8782ba66b9773d80cad4
Noam Meltzer <tsnoam@gmail.com>
2015-11-27 11:23:38 +0200

    send_msg() & send_sticker() methods

--- breakfast.py
+++ breakfast.py
@@ -1,7 +1,8 @@
 from ConfigParser import ConfigParser
-
 import telegram
 import time
+
+RELEASE_KEYBOARD = telegram.ReplyKeyboardHide()
 
 
 class Breakfast(object):
@@ -20,29 +21,42 @@ class Breakfast(object):
         self.chat_id = cfg.getint('global', 'chat_id')
         self.timeout = cfg.getint('global', 'timeout')  # in seconds
 
+    def send_msg(self, text, reply_markup=None):
+        kwargs = dict(chat_id=self.chat_id, text=text)
+        if reply_markup:
+            kwargs['reply_markup'] = reply_markup
+        return self.bot.sendMessage(**kwargs)
+
+    def send_sticker(self, id_, reply_markup=None):
+        kwargs = dict(chat_id=self.chat_id, sticker=id_)
+        if reply_markup:
+            kwargs['reply_markup'] = reply_markup
+        return self.bot.sendSticker(**kwargs)
+
     def smack_these_etas(self):
-        self.bot.sendMessage(chat_id=self.chat_id, text='ETAs')
-        self.bot.sendMessage(chat_id=self.chat_id, text='john: 7:25')
-        self.bot.sendMessage(chat_id=self.chat_id, text='sagi: 8:08')
-        self.bot.sendMessage(chat_id=self.chat_id, text='noam: >=10')
-        self.bot.sendMessage(chat_id=self.chat_id, text='yoav: wait for me guys!')
-        self.bot.sendSticker(chat_id=self.chat_id, sticker='BQADAwADNQADYK6GBTswjvGbLJgxAg')
-        self.bot.sendMessage(chat_id=self.chat_id, text='noam: john, gila tomorrow?')
-        self.bot.sendMessage(chat_id=self.chat_id, text='sagi:')
-        self.bot.sendSticker(chat_id=self.chat_id, sticker='BQADBAADcgADD4KaAAEfqtBiFqveBQI')
+        self.send_msg('ETAs')
+        self.send_msg('john: 7:25')
+        self.send_msg('sagi: 8:08')
+        self.send_msg('noam: >=10')
+        self.send_msg('yoav: wait for me guys!')
+        self.send_sticker('BQADAwADNQADYK6GBTswjvGbLJgxAg')
+        self.send_msg('noam: john, gila tomorrow?')
+        self.send_msg('sagi:')
+        self.send_sticker('BQADBAADcgADD4KaAAEfqtBiFqveBQI')
+
+    def gila_day(self):
+        pass
 
     def normal_day(self):
         here_msgs = ['here!', '7:25', '8:08']
         loser_msgs = ["I'm a loser!"]
         here = set()
         losers = set()
+        custom_keyboard = [here_msgs[:2], here_msgs[2:] + ['>=10'], loser_msgs]
+        reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
 
         self.clean_srv_msgs()
-
-        custom_keyboard = [here_msgs + ['>=10'] + loser_msgs]
-        reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-        self.bot.sendMessage(chat_id=self.chat_id, text='ETAs?', reply_markup=reply_markup)
-
+        self.send_msg('ETAs?', reply_markup)
         time.sleep(self.timeout)
         updates = self.bot.getUpdates()
 
@@ -53,25 +67,26 @@ class Breakfast(object):
                 losers.add(self.get_name(i.message.from_user))
 
         if len(here) == 0:
-            self.bot.sendSticker(chat_id=self.chat_id, sticker='BQADAwADegADv4yQBIEb_nJ9h1mRAg',
-                                 reply_markup=telegram.ReplyKeyboardHide())
+            self.send_sticker('BQADAwADegADv4yQBIEb_nJ9h1mRAg', reply_markup=RELEASE_KEYBOARD)  # crying blond
+            self.send_msg('no one is coming!!! :-(')
         elif len(here) == 1:
-            self.bot.sendMessage(chat_id=self.chat_id, text='{}, may the force be with you'.format(list(here)[0]),
-                                 reply_markup=telegram.ReplyKeyboardHide())
+            self.send_msg('{}, may the force be with you'.format(list(here)[0]), reply_markup=RELEASE_KEYBOARD)
+            self.send_sticker('BQADAQADKwEAAtpxZgcxjbgZ2PsfdwI')  # luke skywalker - stand on one hand
         else:
-            self.bot.sendMessage(chat_id=self.chat_id, text='{} brogrammers gonna have fun'.format(len(here)),
-                                 reply_markup=telegram.ReplyKeyboardHide())
+            self.send_msg('{} brogrammers gonna have fun'.format(len(here)), reply_markup=RELEASE_KEYBOARD)
 
         if len(losers) > 0:
-            self.bot.sendMessage(chat_id=self.chat_id, text='It appears that {} {} low self esteem'.format(
-                self.comma_names(losers), 'has' if len(losers) == 1 else 'have'))
+            self.send_msg('It appears that {} {} low self esteem'.format(self.comma_names(losers),
+                                                                         'has' if len(losers) == 1 else 'have'))
+            self.send_sticker('BQADBAADjAADD4KaAAFQZH8V3rLRwAI')  # spiderman - look and him and laugh
 
     def clean_srv_msgs(self):
         updates = self.bot.getUpdates()
         while len(updates) > 0:
             updates = self.bot.getUpdates(updates[-1].update_id + 1)
 
-    def get_name(self, chat):
+    @staticmethod
+    def get_name(chat):
         """
         :type chat: telegram.user.User
         :rtype: str
@@ -104,6 +119,7 @@ def main():
     breakfast = Breakfast('breakfast.conf')
     # breakfast.smack_these_etas()
     breakfast.normal_day()
+
 
 if __name__ == '__main__':
     main()
