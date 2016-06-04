a0521185fa7fb15a510140b95d176df9bee89208
John Basila <jbasila@gmail.com>
2016-06-04 18:29:09 +0300

    this is an incomplete commit

--- new_breakfast.py
+++ new_breakfast.py
@@ -4,7 +4,9 @@ from telegram import ReplyKeyboardMarkup, ReplyKeyboardHide
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 import argparse
 from configparser import ConfigParser
+from datetime import datetime
 
+WILL_JOIN_OPTIONS = ['Here', '7:30', '8:00', '8:30', '9:00', '9:30']
 
 class EtaChat(object):
     token = None
@@ -17,8 +19,11 @@ class EtaChat(object):
         self.token = token
         self.chat_id = chat_id
         self.admin_chat_id = admin_chat_id
-        self.start_time = start_time
-        self.end_time = end_time
+        self.start_time = start_time.split(':')
+        self.end_time = end_time.split(':')
+
+        self._startTimeInt = int(self.start_time[0]) * 60 + int(self.start_time[1])
+        self._endTimeInt = int(self.end_time[0]) * 60 + int(self.end_time[1])
 
         self.eta_collection_on = False
 
@@ -34,20 +39,20 @@ class EtaChat(object):
         self.updater.dispatcher.add_handler(CommandHandler('begin', self.command_begin))
         self.updater.dispatcher.add_handler(CommandHandler('send', self.command_send))
         self.updater.dispatcher.add_handler(CommandHandler('end', self.command_end))
-        self.updater.dispatcher.add_handler(MessageHandler([Filters.text], self.set_value))
+        self.updater.dispatcher.add_handler(MessageHandler([Filters.text], self.message_received))
 
     def do_begin_eta_collection(self):
         if not self.eta_collection_on:
-            self.updater.bot.sendMessage(self.chat_id,
-                                         text="ETA?",
-                                         reply_markup=self.reply_markup)
+            self.updater.bot.send_message(self.chat_id,
+                                          text="ETA?",
+                                          reply_markup=self.reply_markup)
         self.eta_collection_on = True
 
     def do_end_eta_collection(self):
         if self.eta_collection_on:
-            self.updater.bot.sendMessage(chat_id=self.chat_id,
-                                         text='Done for now, here is the status:',
-                                         reply_markup=ReplyKeyboardHide())
+            self.updater.bot.send_message(chat_id=self.chat_id,
+                                          text='Done for now, here is the status:',
+                                          reply_markup=ReplyKeyboardHide())
         self.eta_collection_on = False
 
     def do_start(self, bot, update):
@@ -55,12 +60,32 @@ class EtaChat(object):
 
     def do_help(self, update):
         message = ''
-        self.updater.bot.sendMessage()
+        self.updater.bot.send_message()
 
     def command_start(self, bot, update):
-        print(update.message.chat.id)
-        print(update.message.chat)
-        print(update.message)
+        _message_reply = None
+        if update.message.chat.id == self.admin_chat_id:
+            _message_reply = 'Welcome master'
+        else:
+            # is this Noam?
+            if update.message.chat.username == 'tsnoam':
+                _message_reply = 'I usually tend to ignore or blow ' \
+                                 'others away but my master told me to be polite ' \
+                                 'to you specifically Mr. Noam. So I will be polite ' \
+                                 'and tell you that I have only one master and that ' \
+                                 'is not you :). You have a great day now! I will be ' \
+                                 'ignoring messages now.'
+
+            self.updater.bot.send_message(chat_id=self.admin_chat_id,
+                                          text='Username ({} {} - @{}, chat_id = {}), '
+                                               'tried to contact me'.format(update.message.chat.first_name,
+                                                                            update.message.chat.last_name,
+                                                                            update.message.chat.username,
+                                                                            update.message.chat_id))
+
+        if _message_reply is not None:
+            self.updater.bot.send_message(chat_id=update.message.chat.id,
+                                          text=_message_reply)
 
     def command_help(self, bot, update):
         pass
@@ -71,23 +96,51 @@ class EtaChat(object):
 
     def command_send(self, bot, update):
         if update.message.chat.id == self.admin_chat_id:
-            self.updater.bot.sendMessage(self.chat_id,
-                                         text=update.message.text[5:])
+            self.updater.bot.send_message(self.chat_id,
+                                          text=update.message.text[5:])
 
     def command_end(self, bot, update):
         if update.message.chat.id == self.admin_chat_id:
             self.do_end_eta_collection()
 
-    def set_value(self, bot, update):
-        print(update)
+    def message_received(self, bot, update):
+        # print(update.message)
+        c = self.custom_keyboard.flatten()
+        print(c)
+        print(c)
+        # if update.message.chat_id == self.chat_id:
+        #     if self.eta_collection_on:
+        #         pass
 
     def run(self):
         def beep(bot):
-            self.do_begin_eta_collection()
+            _now = datetime.now()
+            _nowTimeInt = int(_now.hour) * 60 + int(_now.minute)
 
-        # self.updater.job_queue.put(beep, 0, repeat=False)
+            if _now.isoweekday() > 4:
+                # Weekend, not doing notification
+                return
+
+            # Check if should start collecting time
+            if self.eta_collection_on:
+                # check if reached end of collection
+                if self._startTimeInt <= _nowTimeInt <= self._endTimeInt:
+                    # Still collecting
+                    pass
+                else:
+                    # Reached end of collection, notify and let's do a summary.
+                    self.do_end_eta_collection()
+            else:
+                # check if in time frame
+                if self._startTimeInt <= _nowTimeInt <= self._endTimeInt:
+                    self.do_begin_eta_collection()
+
+        self.updater.job_queue.put(beep, 1, repeat=True)
         # Start the Bot
         self.updater.start_polling()
+
+        self.updater.bot.send_message(self.admin_chat_id,
+                                      text='Bot is now online')
 
         # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
         # SIGTERM or SIGABRT
